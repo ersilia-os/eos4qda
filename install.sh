5fb4de8361fe6a1a0d407f4a798db6ae9596e43a
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2023-11-03 19:25:42 +0100

    updating readme [skip ci]

--- install.sh
+++ install.sh
@@ -12,10 +12,11 @@ try_sudo() {
 try_sudo apt-get update
 try_sudo apt-get install -y apt-utils
 try_sudo apt-get install -y software-properties-common
+try_sudo wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh
+try_sudo sh ./opam_installer.sh /usr/local/bin
 #try_sudo add-apt-repository ppa:avsm/ppa
 #try_sudo apt-get update
 #try_sudo apt-get install -y opam
-conda install -c conda-forge opam -y
 opam init -y
 eval $(opam env --switch=default)
 pip install rdkit

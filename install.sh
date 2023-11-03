c8dc573b96c45185bb3bc180d246f81a51ec65b8
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2023-11-03 13:20:36 +0100

    updating readme [skip ci]

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
-if [ -f /.dockerenv ]; then
-    apt-get update
-    apt-get install software-properties-common
-    add-apt-repository ppa:avsm/ppa
-    apt-get update
-    apt-get install opam
-else
-    sudo apt-get update
-    sudo apt-get install software-properties-common
-    sudo add-apt-repository ppa:avsm/ppa
-    sudo apt-get update
-    sudo apt-get install opam
-fi
+try_sudo() {
+  cmd="$@"
+  if command -v sudo >/dev/null 2>&1; then
+    # If sudo is available, try to run the command with sudo.
+    sudo $cmd || $cmd
+  else
+    # If sudo is not available, run the command without sudo.
+    $cmd
+  fi
+}
+try_sudo apt-get update
+try_sudo apt-get install software-properties-common
+try_sudo add-apt-repository ppa:avsm/ppa
+try_sudo apt-get update
+try_sudo apt-get install opam
 opam init -y
 eval $(opam env --switch=default)
 pip install rdkit

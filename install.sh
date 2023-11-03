27c5a6d03bc22683b7f4adb1974235e662fa68a6
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2023-11-03 07:15:37 -0300

    updating readme [skip ci]

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-sudo apt-get update
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:avsm/ppa
-sudo apt-get update
-sudo apt-get install opam
+if [ -f /.dockerenv ]; then
+    apt-get update
+    apt-get install software-properties-common
+    add-apt-repository ppa:avsm/ppa
+    apt-get update
+    apt-get install opam
+else
+    sudo apt-get update
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository ppa:avsm/ppa
+    sudo apt-get update
+    sudo apt-get install opam
+fi
 opam init -y
 eval $(opam env --switch=default)
 pip install rdkit

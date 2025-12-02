6706ee29783f4ed38a6970352443cc5cbf5d3469
Abel Legese <73869888+Abellegese@users.noreply.github.com>
2025-12-02 20:14:24 +0300

    Refactor opam_install.sh to use try_sudo function [skip ci]

--- opam_install.sh
+++ opam_install.sh
@@ -1,15 +1,31 @@
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
-apt-get install -y opam bubblewrap m4 unzip git curl ca-certificates python3-pip
+try_sudo() {
+  cmd="$@"
+  if command -v sudo >/dev/null 2>&1; then
+    sudo $cmd || $cmd
+  else
+    $cmd
+  fi
+}
+
+try_sudo apt-get update
+try_sudo apt-get install -y autoconf apt-utils software-properties-common curl ca-certificates
+
+curl -fsSL https://opam.ocaml.org/install.sh -o /tmp/opam-install.sh
+
+try_sudo sh /tmp/opam-install.sh --no-backup
+
+export PATH="/usr/local/bin:$PATH"
 
 opam init --disable-sandboxing -y
+eval "$(opam env --switch=default)"
+
+python -m pip install rdkit==2023.9.6
+
 eval "$(opam env)"
-
-python3 -m pip install rdkit==2023.9.6
-
-opam install -y conf-rdkit
+opam install --fake conf-rdkit
 opam install -y fasmifra
 
-which fasmifra
-which fasmifra_fragment.py
+which fasmifra_fragment.py || echo "fasmifra_fragment.py not found"
+which fasmifra || echo "fasmifra not found"

a6a2f998f7ec2622ac0b2ca80b31524e80cb7367
Abel Legese <73869888+Abellegese@users.noreply.github.com>
2025-12-02 18:44:39 +0300

    Refactor opam_install.sh for simplicity and updates [skip ci]
    Removed try_sudo function and simplified package installation commands. Updated python command to python3 and made other minor adjustments.

--- opam_install.sh
+++ opam_install.sh
@@ -1,31 +1,15 @@
 export DEBIAN_FRONTEND=noninteractive
 
-try_sudo() {
-  cmd="$@"
-  if command -v sudo >/dev/null 2>&1; then
-    sudo $cmd || $cmd
-  else
-    $cmd
-  fi
-}
-
-try_sudo apt-get update
-try_sudo apt-get install -y autoconf apt-utils software-properties-common curl ca-certificates
-
-curl -fsSL https://opam.ocaml.org/install.sh -o /tmp/opam-install.sh
-sh /tmp/opam-install.sh --download-only
-OPAM_BIN="$(ls /tmp/opam-*-x86_64-linux | head -n 1)"
-try_sudo install "$OPAM_BIN" /usr/local/bin/opam
-export PATH="/usr/local/bin:$PATH"
+apt-get update
+apt-get install -y opam bubblewrap m4 unzip git curl ca-certificates python3-pip
 
 opam init --disable-sandboxing -y
-eval "$(opam env --switch=default)"
-
-python -m pip install rdkit==2023.9.6
-
 eval "$(opam env)"
-opam install --fake conf-rdkit
+
+python3 -m pip install rdkit==2023.9.6
+
+opam install -y conf-rdkit
 opam install -y fasmifra
 
-which fasmifra_fragment.py
 which fasmifra
+which fasmifra_fragment.py
